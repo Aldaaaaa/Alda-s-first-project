0ac1d3470e23e7b6fe8b0d820b2f2bc1438e38a4
Aldaaaaa <Aldaaaaa>
2024-07-17 16:25:59 -0600

    mi primer commit

---
+++ DevFile.py
@@ -0,0 +1,60 @@
+def main():
+    print('opciones: suma(1) resta(2) multiplicacion(3) division(4)')
+    operacion = int(input('Introduce tu operacion: '))
+    if operacion == 1:
+        num1 = int(input('Introduce tu primer digito: '))
+        num2 = int(input('Introduce tu segundo digito: '))
+        print(num1 + num2)
+    if operacion == 2:
+        num1 = int(input('Introduce tu primer digito: '))
+        num2 = int(input('Introduce tu segundo digito: '))
+        print(num1 - num2)
+    if operacion == 3:
+        num1 = int(input('Introduce tu primer digito: '))
+        num2 = int(input('Introduce tu segundo digito: '))
+        print(num1 * num2)
+    if operacion == 4:
+        num1 = int(input('Introduce tu primer digito: '))
+        num2 = int(input('Introduce tu segundo digito: '))
+        if num2 == 0:
+            return print('No perro')
+        print(num1 / num2)
+
+#from Aldaaaaa
+
+
+
+
+
+# def insert():
+#     num1 = int(input('Introduce tu primer digito: '))
+#     num2 = int(input('Introduce tu segundo digito: '))
+
+#     return num1, num2
+
+
+# def main():
+#     print('opciones: suma(1) resta(2) multiplicacion(3) division(4)')
+#     operacion = int(input('Introduce tu operacion: '))
+
+#     num1, num2 = insert()
+    
+#     if operacion == 1:
+#         print(num1 + num2)
+
+#     if operacion == 2:
+#         print(num1 - num2)
+
+#     if operacion == 3:
+#         print(num1 * num2)
+
+#     if operacion == 4:
+#         if num2 == 0:
+#             return print('No perro')
+#         print(num1 / num2)
+    
+# main()
+
+
+
+#from AlexisLara
